36203345a9cfd9a43e73c8154934274de129ea35
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-04 17:45:12 -0400

    23 tests passed

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -7,7 +9,7 @@ public class StringArrayUtils {
     /**
      * @param array array of String objects
      * @return first element of specified array
-     */ // TODO
+     */ // TODOdone
     public static String getFirstElement(String[] array) {
         return array[0];
     }
@@ -25,7 +27,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +35,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
@@ -42,15 +44,31 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                return true;
+            }
+        }
         return false;
     }
+
 
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        // Create a new array to store the reversed elements
+        String[] reverseArray = new String[array.length];
+        // Initialize a counter variable to keep track of the position in the reverseArray
+        int count = 0;
+        for (int i = array.length - 1; i >= 0; i--) {//original array from the last element to the first element
+            reverseArray[count] = array[i];// Assign each element of the original array to the reverseArray
+            // Starting from the last element to the first element
+            count++;// Increment the count to move to the next position in reverseArray
+
+        }
+        return reverseArray;
     }
 
     /**
@@ -58,7 +76,18 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        //create two counters that start from both ends
+        int count = 0;
+        int countReverse = array.length - 1;
+        while (count < countReverse) {// iterates through the array until both meet in teh middle
+            if (array[count].equals(array[countReverse])) {
+                //if the elements at the opposite side are equal, it is a palindrome
+                return true;
+            }
+            count++; //move each counter towards each other
+            countReverse--;
+        }
+        return false;//if they are not equal, it is not a palindrome
     }
 
     /**
@@ -75,7 +104,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int numOfOcc = 0; //initialize #of occurances to zero
+        for (int i = 0; i < array.length; i++) {// loop through the array
+            if (array[i].indexOf(value) != -1) {//check if element contains 'value'. if value not found, it
+                numOfOcc++; //increment number of occurances everytime value is found
+            }
+        }
+        return numOfOcc;
     }
 
     /**
@@ -84,8 +119,15 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
+//        int index = array.indexOf(valueToRemove);
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i].equals(valueToRemove)) {
+//                array[i] = "";
+//                break;
+//            }
         return null;
     }
+
 
     /**
      * @param array array of chars
